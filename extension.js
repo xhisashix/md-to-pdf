096cb765e01c079a3fba7c91af11600507340e5c
Hisashi Iwamoto <hisasi622@gmail.com>
2023-05-20 10:55:45 +0900

    PDFファイルの作成

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const markdown_pdf = require("markdown-pdf")
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,8 +18,18 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('md-to-pdf.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('md-to-pdf.export', function () {
 		// The code you place here will be executed every time your command is executed
+
+		// Get the active text editor
+		const editor = vscode.window.activeTextEditor;
+
+		// Get file name
+		const file = editor.document.fileName.split(".")[0]
+		// generate pdf
+		markdown_pdf().from.string(editor.document.getText()).to(file + ".pdf", function () {
+			console.log("Done")
+		})
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from md-to-pdf!');
